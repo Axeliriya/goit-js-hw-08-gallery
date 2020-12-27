ae2cc4184365c312c40a838b9a452b7630341055
Axeliriya <axeliriya@mail.ru>
2020-12-27 18:26:20 +0200

    сделала карусель

--- js/index.js
+++ js/index.js
@@ -7,11 +7,24 @@ const lightboxButton = document.querySelector(
 );
 const lightboxOverlayRef = document.querySelector('.lightbox__overlay');
 const lightboxImageRef = document.querySelector('.lightbox__image');
+const lightboxContent = document.querySelector('.lightbox__content');
 let itemRef = '';
 
+const buttonLeft = document.createElement('button');
+const buttonRight = document.createElement('button');
+buttonLeft.type = 'button';
+buttonRight.type = 'button';
+buttonLeft.classList.add('lightbox__btn--left', 'lightbox__btn');
+buttonRight.classList.add('lightbox__btn--right', 'lightbox__btn');
+
+lightboxContent.append(buttonLeft, buttonRight);
+
+const buttonRightRef = document.querySelector('.lightbox__btn--right');
+const buttonLeftRef = document.querySelector('.lightbox__btn--left');
+
 galleryItems.forEach(
-  item =>
-    (itemRef += `<li class='gallery__item'><img class='gallery__image' src='${item.original}' alt='${item.description}'></img></li>`),
+  (item, i) =>
+    (itemRef += `<li class='gallery__item'><img class='gallery__image' src='${item.original}' alt='${item.description}' data-index='${i}'></img></li>`),
 );
 
 galleryRef.insertAdjacentHTML('beforeend', itemRef);
@@ -25,12 +38,47 @@ const openImageInBackdrop = () => {
     }
   });
   lightboxRef.classList.add('is-open');
+  let currentIndex = +lightboxImageRef.dataset.index;
+  let currentImg = lightboxImageRef.src;
+  let nextImg;
+  buttonRightRef.addEventListener('click', () => {
+    if (currentIndex >= 0 && currentIndex < galleryImage.length - 1) {
+      currentIndex += 1;
+      nextImg = document.querySelector(`img[data-index='${currentIndex}']`).src;
+      currentImg = nextImg;
+      lightboxImageRef.src = currentImg;
+      lightboxImageRef.dataset.index = currentIndex;
+    } else {
+      currentIndex = 0;
+      nextImg = document.querySelector(`img[data-index='${currentIndex}']`).src;
+      currentImg = nextImg;
+      lightboxImageRef.src = currentImg;
+    }
+  });
+  buttonLeftRef.addEventListener('click', () => {
+    if (currentIndex > 0 && currentIndex <= galleryImage.length) {
+      buttonLeftRef.disabled = false;
+      currentIndex -= 1;
+      const nextImg = document.querySelector(
+        `img[data-index='${currentIndex}']`,
+      ).src;
+      currentImg = nextImg;
+      lightboxImageRef.src = currentImg;
+      lightboxImageRef.dataset.index = currentIndex;
+    } else {
+      currentIndex = galleryImage.length - 1;
+      nextImg = document.querySelector(`img[data-index='${currentIndex}']`).src;
+      currentImg = nextImg;
+      lightboxImageRef.src = currentImg;
+    }
+  });
 };
 
 galleryImage.forEach(image => {
   image.addEventListener('click', () => {
-    openImageInBackdrop();
     lightboxImageRef.src = image.src;
+    lightboxImageRef.dataset.index = image.dataset.index;
+    openImageInBackdrop();
   });
 });
 
