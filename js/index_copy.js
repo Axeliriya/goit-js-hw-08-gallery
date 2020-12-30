d67abc23acbeaaba4e61014a02706a0ab2d501a8
Axeliriya <axeliriya@mail.ru>
2020-12-30 11:11:31 +0200

    Рефакторинг слайдера

--- js/index_copy.js
+++ js/index_copy.js
@@ -10,6 +10,7 @@ const refs = {
   lightboxImage: document.querySelector('.lightbox__image'),
   lightboxContent: document.querySelector('.lightbox__content'),
 };
+
 let counterImg = '';
 galleryItems.forEach(
   (item, i) =>
@@ -29,9 +30,24 @@ galleryItems.forEach(
   </li>`),
 );
 
-refs.gallery.insertAdjacentHTML('beforeend', counterImg);
+const createBtn = () => {
+  const btnLeft = document.createElement('button');
+  const btnRight = document.createElement('button');
+  btnLeft.type = 'button';
+  btnRight.type = 'button';
+  btnLeft.classList.add('lightbox__btn--left', 'lightbox__btn');
+  btnRight.classList.add('lightbox__btn--right', 'lightbox__btn');
 
-const openModalHendler = event => {
+  refs.lightboxContent.append(btnLeft, btnRight);
+};
+
+refs.gallery.insertAdjacentHTML('beforeend', counterImg);
+createBtn();
+
+const btnLeftEl = document.querySelector('.lightbox__btn--left');
+const btnRightEl = document.querySelector('.lightbox__btn--right');
+
+const openModalHandler = event => {
   event.preventDefault();
   if (event.target.nodeName !== 'IMG') {
     return;
@@ -41,12 +57,15 @@ const openModalHendler = event => {
   refs.lightbox.classList.add('is-open');
   changeAtribute(currentImg);
 
-  refs.btnCloseLightbox.addEventListener('click', closeModalHendler);
-  window.addEventListener('keydown', closeEscHendler);
-  refs.lightboxOverlay.addEventListener('click', closeOverlayHendler);
+  refs.btnCloseLightbox.addEventListener('click', closeModalHandler);
+  refs.lightboxOverlay.addEventListener('click', closeOverlayHandler);
+  btnRightEl.addEventListener('click', clickBtnHandler);
+  btnLeftEl.addEventListener('click', clickBtnHandler);
+  window.addEventListener('keydown', closeEscHandler);
+  window.addEventListener('keydown', downArrowHandler);
 };
 
-refs.gallery.addEventListener('click', openModalHendler);
+refs.gallery.addEventListener('click', openModalHandler);
 
 const changeAtribute = item => {
   refs.lightboxImage.src = item.dataset.source;
@@ -54,19 +73,51 @@ const changeAtribute = item => {
   refs.lightboxImage.dataset.index = item.dataset.index;
 };
 
-const closeModalHendler = () => {
+const closeModalHandler = () => {
   refs.lightbox.classList.remove('is-open');
   refs.lightboxImage.src = '';
 };
 
-const closeOverlayHendler = event => {
+const closeOverlayHandler = event => {
   if (event.target === event.currentTarget) {
-    closeModalHendler();
+    closeModalHandler();
   }
 };
 
-const closeEscHendler = event => {
+const closeEscHandler = event => {
   if (event.code === 'Escape') {
-    closeModalHendler();
+    closeModalHandler();
+  }
+};
+
+const nextImg = i => {
+  if (i < 0) {
+    i = refs.gallery.children.length - 1;
+  }
+  if (i > refs.gallery.children.length - 1) {
+    i = 0;
+  }
+
+  const nextImg = document.querySelector(`img[data-index='${i}']`);
+  changeAtribute(nextImg);
+};
+
+const downArrowHandler = event => {
+  const currentIdx = +refs.lightboxImage.dataset.index;
+  if (event.code === 'ArrowLeft') {
+    nextImg(currentIdx - 1);
+  }
+  if (event.code === 'ArrowRight') {
+    nextImg(currentIdx + 1);
+  }
+};
+
+const clickBtnHandler = event => {
+  const currentIdx = +refs.lightboxImage.dataset.index;
+  if (event.target.classList.contains('lightbox__btn--left')) {
+    nextImg(currentIdx - 1);
+  }
+  if (event.target.classList.contains('lightbox__btn--right')) {
+    nextImg(currentIdx + 1);
   }
 };
