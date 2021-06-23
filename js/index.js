44dff62571cb08afab127950042562ef306b2c42
Axeliriya <axeliriya@mail.ru>
2021-06-23 09:19:34 +0200

    удалила лишние файлы

--- js/index.js
+++ js/index.js
@@ -1,94 +1,186 @@
 import galleryItems from './gallery-items.js';
 
-const lightboxRef = document.querySelector('.lightbox');
-const galleryRef = document.querySelector('.gallery');
-const lightboxButton = document.querySelector(
-  'button[data-action="close-lightbox"]',
-);
-const lightboxOverlayRef = document.querySelector('.lightbox__overlay');
-const lightboxImageRef = document.querySelector('.lightbox__image');
-const lightboxContent = document.querySelector('.lightbox__content');
-let itemRef = '';
+const refs = {
+  lightbox: document.querySelector('.lightbox'),
+  gallery: document.querySelector('.gallery'),
+  btnCloseLightbox: document.querySelector(
+    'button[data-action="close-lightbox"]',
+  ),
+  lightboxOverlay: document.querySelector('.lightbox__overlay'),
+  lightboxImage: document.querySelector('.lightbox__image'),
+  lightboxContent: document.querySelector('.lightbox__content'),
+  progress: document.querySelector('.progress'),
+};
 
-const buttonLeft = document.createElement('button');
-const buttonRight = document.createElement('button');
-buttonLeft.type = 'button';
-buttonRight.type = 'button';
-buttonLeft.classList.add('lightbox__btn--left', 'lightbox__btn');
-buttonRight.classList.add('lightbox__btn--right', 'lightbox__btn');
-
-lightboxContent.append(buttonLeft, buttonRight);
-
-const buttonRightRef = document.querySelector('.lightbox__btn--right');
-const buttonLeftRef = document.querySelector('.lightbox__btn--left');
-
+let counterImg = '';
 galleryItems.forEach(
   (item, i) =>
-    (itemRef += `<li class='gallery__item'><img class='gallery__image' src='${item.original}' alt='${item.description}' data-index='${i}'></img></li>`),
+    (counterImg += `<li class="gallery__item">
+    <a
+      class="gallery__link"
+      href="${item.original}"
+    >
+      <img
+        class="gallery__image"
+        data-lazy="${item.preview}"
+        data-source="${item.original}"
+        data-index="${i}"
+        alt="${item.description}"
+      />
+    </a>
+  </li>`),
 );
 
-galleryRef.insertAdjacentHTML('beforeend', itemRef);
+const createBtn = () => {
+  const btnLeft = document.createElement('button');
+  const btnRight = document.createElement('button');
+  const boxInput = document.createElement('div');
 
-const galleryImage = galleryRef.querySelectorAll('.gallery__image');
+  btnLeft.type = 'button';
+  btnRight.type = 'button';
 
-const openImageInBackdrop = () => {
-  window.addEventListener('keydown', event => {
-    if (event.code === 'Escape') {
-      onCloseBackdrop();
-    }
-  });
-  lightboxRef.classList.add('is-open');
-  let currentIndex = +lightboxImageRef.dataset.index;
-  let currentImg = lightboxImageRef.src;
-  let nextImg;
-  buttonRightRef.addEventListener('click', () => {
-    if (currentIndex >= 0 && currentIndex < galleryImage.length - 1) {
-      currentIndex += 1;
-      nextImg = document.querySelector(`img[data-index='${currentIndex}']`).src;
-      currentImg = nextImg;
-      lightboxImageRef.src = currentImg;
-      lightboxImageRef.dataset.index = currentIndex;
-    } else {
-      currentIndex = 0;
-      nextImg = document.querySelector(`img[data-index='${currentIndex}']`).src;
-      currentImg = nextImg;
-      lightboxImageRef.src = currentImg;
-    }
-  });
-  buttonLeftRef.addEventListener('click', () => {
-    if (currentIndex > 0 && currentIndex <= galleryImage.length) {
-      currentIndex -= 1;
-      const nextImg = document.querySelector(
-        `img[data-index='${currentIndex}']`,
-      ).src;
-      currentImg = nextImg;
-      lightboxImageRef.src = currentImg;
-      lightboxImageRef.dataset.index = currentIndex;
-    } else {
-      currentIndex = galleryImage.length - 1;
-      nextImg = document.querySelector(`img[data-index='${currentIndex}']`).src;
-      currentImg = nextImg;
-      lightboxImageRef.src = currentImg;
-    }
-  });
+  btnLeft.classList.add('lightbox__btn--left', 'lightbox__btn');
+  btnRight.classList.add('lightbox__btn--right', 'lightbox__btn');
+  boxInput.classList.add('box__input');
+
+  refs.lightboxContent.append(btnLeft, btnRight, boxInput);
 };
 
-galleryImage.forEach(image => {
-  image.addEventListener('click', () => {
-    lightboxImageRef.src = image.src;
-    lightboxImageRef.dataset.index = image.dataset.index;
-    openImageInBackdrop();
-  });
-});
+refs.gallery.insertAdjacentHTML('beforeend', counterImg);
+createBtn();
 
-const onCloseBackdrop = () => {
-  lightboxRef.classList.remove('is-open');
-  lightboxImageRef.src = '';
-};
+let lableRadio = '';
+galleryItems.forEach(
+  (item, i) =>
+    (lableRadio += `<label class="box-input__lable" id="img">
+    <input class="box-input__input" type="radio" name="img" data-idx="${i}" />
+  </label>`),
+);
 
-lightboxButton.addEventListener('click', onCloseBackdrop);
-lightboxOverlayRef.addEventListener('click', event => {
-  if (event.target === event.currentTarget) {
-    onCloseBackdrop();
+const btnLeftEl = document.querySelector('.lightbox__btn--left');
+const btnRightEl = document.querySelector('.lightbox__btn--right');
+const box = document.querySelector('.box__input');
+
+box.insertAdjacentHTML('beforeend', lableRadio);
+
+const openModalHandler = event => {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
   }
+
+  const currentImg = event.target;
+  refs.lightbox.classList.add('is-open');
+  changeAtribute(currentImg);
+  currentRadioBatton();
+
+  refs.btnCloseLightbox.addEventListener('click', closeModalHandler);
+  refs.lightboxOverlay.addEventListener('click', closeOverlayHandler);
+  btnRightEl.addEventListener('click', clickBtnHandler);
+  btnLeftEl.addEventListener('click', clickBtnHandler);
+  box.addEventListener('input', checkInput);
+  window.addEventListener('keydown', closeEscHandler);
+  window.addEventListener('keydown', downArrowHandler);
+  document.body.style.overflow = 'hidden';
+};
+
+refs.gallery.addEventListener('click', openModalHandler);
+
+const progressBar = () => {
+  let scroll = document.body.scrollTop || document.documentElement.scrollTop;
+  let height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let per = (scroll / height) * 100;
+
+  refs.progress.style.width = `${per}%`;
+};
+window.addEventListener('scroll', progressBar);
+
+const changeAtribute = item => {
+  refs.lightboxImage.src = item.dataset.source;
+  refs.lightboxImage.alt = item.alt;
+  refs.lightboxImage.dataset.index = item.dataset.index;
+};
+
+const currentRadioBatton = () => {
+  const currentIdx = +refs.lightboxImage.dataset.index;
+  const dataCheck = document.querySelector(`[data-idx='${currentIdx}']`);
+  dataCheck.checked = true;
+};
+
+const closeModalHandler = () => {
+  refs.lightbox.classList.remove('is-open');
+  refs.lightboxImage.src = '';
+  document.body.style.overflow = 'auto';
+};
+
+const closeOverlayHandler = event => {
+  if (event.target === event.currentTarget) {
+    closeModalHandler();
+  }
+};
+
+const closeEscHandler = event => {
+  if (event.code === 'Escape') {
+    closeModalHandler();
+  }
+};
+
+const nextImg = i => {
+  if (i < 0) {
+    i = refs.gallery.children.length - 1;
+  }
+  if (i > refs.gallery.children.length - 1) {
+    i = 0;
+  }
+
+  const nextImg = document.querySelector(`img[data-index='${i}']`);
+  changeAtribute(nextImg);
+  currentRadioBatton();
+};
+
+const downArrowHandler = event => {
+  const currentIdx = +refs.lightboxImage.dataset.index;
+  if (event.code === 'ArrowLeft') {
+    nextImg(currentIdx - 1);
+  }
+  if (event.code === 'ArrowRight') {
+    nextImg(currentIdx + 1);
+  }
+};
+
+const clickBtnHandler = event => {
+  const currentIdx = +refs.lightboxImage.dataset.index;
+  if (event.target.classList.contains('lightbox__btn--left')) {
+    nextImg(currentIdx - 1);
+  }
+  if (event.target.classList.contains('lightbox__btn--right')) {
+    nextImg(currentIdx + 1);
+  }
+};
+
+const checkInput = event => {
+  if (event.target.nodeName !== 'INPUT') {
+    return;
+  }
+  const nextCheck = document.querySelector('input[name="img"]:checked');
+  const nextIdx = +nextCheck.dataset.idx;
+
+  nextImg(nextIdx);
+};
+
+const galleryImage = document.querySelectorAll('.gallery__image');
+
+const io = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      const image = entry.target;
+      const src = image.dataset.lazy;
+
+      image.src = src;
+    }
+  });
 });
+
+galleryImage.forEach(image => io.observe(image));
